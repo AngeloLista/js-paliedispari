526c20305091caba7117941e2df7296b722dc819
AngeloLista <abstractplayer@gmail.com>
2022-01-20 18:21:29 +0100

    Shortened "function return"

--- js/script.js
+++ js/script.js
@@ -4,18 +4,19 @@ Creare una funzione per capire se la parola inserita è palindroma */
 
 let userWord = prompt('Inserisci una parola per verificare se è palindroma').trim();
 
-
 function isPalindrome(word) {
     let reversedWord = '';
     for (let i = word.length - 1; i >= 0; i--) {
         reversedWord += word[i];
     }
 
-    if (word === reversedWord) {
+    /*if (word === reversedWord) {
         return true;
     } else {
         return false;
-    }
+    }*/
+
+    return word === reversedWord ? true : false;
 }
 
 console.log(isPalindrome(userWord))
